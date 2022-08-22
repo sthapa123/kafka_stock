9da18df19d07e9433827c2f35ebea956efe3280a
Suman Thapa <2022.sthapa@gmail.com>
2022-08-21 20:06:01 -0500

    - Run airflow webserver, scheduler and bokeh server in the background at startup for now.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ if [ -z "${BOKEH_PREFIX}" ]; then
 else
     PREFIX_PARAM="--prefix ${BOKEH_PREFIX}";
 fi
+echo "Launching Airflow webserver"
+airflow webserver &
+echo "Launching Airflow scheduler"
+airflow scheduler &
 echo "Launching Bokeh Server..."
 bokeh serve --port ${BOKEH_PORT} --address 0.0.0.0 --allow-websocket-origin "*" ${PREFIX_PARAM} --log-level ${BOKEH_LOG_LEVEL} ~/kafka_stock/pipeline
-echo "Launching Airflow webserver"
-airflow webserver
-echo "Launching Airflow scheduler"
-airflow scheduler
